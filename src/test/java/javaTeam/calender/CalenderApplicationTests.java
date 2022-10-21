085cd79dde0448831ee57010dc339f03cee2e52d
KIMHUEMANG <piyrw9754@gmail.com>
2022-10-21 12:29:08 +0900

    🎉 | project setting

---
+++ src/test/java/javaTeam/calender/CalenderApplicationTests.java
@@ -0,0 +1,13 @@
+package javaTeam.calender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalenderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
