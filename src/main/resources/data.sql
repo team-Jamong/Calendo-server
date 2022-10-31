66c06b52c3ce23253871805a024e4de75a564cf6
piyrw9754@gmail.com <piyrw9754@gmail.com>
2022-10-31 13:50:17 +0900

    member , planner 관계 매핑

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,54 +1,29 @@
 CREATE TABLE `member` (
-                          `member_idx`    int    NOT NULL    COMMENT '멤버 식별 pk',
-                          `login_id`    varchar(40)    NOT NULL    COMMENT '로컬용 아이디',
-                          `password`    varchar(45)    NOT NULL,
-                          `name`    varchar(8)    NOT NULL
-);
-
-CREATE TABLE `planner_content` (
-                                   `content_idx`    int    NOT NULL    COMMENT '내용 식별 pk',
-                                   `member_idx`    int    NOT NULL    COMMENT '멤버 식별 pk',
-                                   `content`    TEXT    NULL    COMMENT '플래너 내용'
+                          `member_idx`	int	NOT NULL	COMMENT '멤버 식별 pk',
+                          `login_id`	varchar(40)	NOT NULL	COMMENT '로컬용 아이디',
+                          `password`	varchar(45)	NOT NULL,
+                          `name`	varchar(8)	NOT NULL
 );
 
 CREATE TABLE `planner` (
-                           `planner_idx`    int    NOT NULL    COMMENT '플래너 식별 pk',
-                           `content_idx`    int    NOT NULL    COMMENT '내용 식별 pk',
-                           `member_idx`    int    NOT NULL    COMMENT '멤버 식별 pk'
+                           `planner_idx`	int	NOT NULL	COMMENT '플래너 식별 pk',
+                           `member_idx`	int	NOT NULL	COMMENT '멤버 식별 pk',
+                           `content`	TEXT	NULL
 );
 
 ALTER TABLE `member` ADD CONSTRAINT `PK_MEMBER` PRIMARY KEY (
                                                              `member_idx`
     );
 
-ALTER TABLE `planner_content` ADD CONSTRAINT `PK_PLANNER_CONTENT` PRIMARY KEY (
-                                                                               `content_idx`,
-                                                                               `member_idx`
-    );
-
 ALTER TABLE `planner` ADD CONSTRAINT `PK_PLANNER` PRIMARY KEY (
                                                                `planner_idx`,
-                                                               `content_idx`,
                                                                `member_idx`
     );
 
-ALTER TABLE `planner_content` ADD CONSTRAINT `FK_member_TO_planner_content_1` FOREIGN KEY (
-                                                                                           `member_idx`
+ALTER TABLE `planner` ADD CONSTRAINT `FK_member_TO_planner_1` FOREIGN KEY (
+                                                                           `member_idx`
     )
     REFERENCES `member` (
                          `member_idx`
         );
 
-ALTER TABLE `planner` ADD CONSTRAINT `FK_planner_content_TO_planner_1` FOREIGN KEY (
-                                                                                    `content_idx`
-    )
-    REFERENCES `planner_content` (
-                                  `content_idx`
-        );
-
-ALTER TABLE `planner` ADD CONSTRAINT `FK_planner_content_TO_planner_2` FOREIGN KEY (
-                                                                                    `member_idx`
-    )
-    REFERENCES `planner_content` (
-                                  `member_idx`
-        );
