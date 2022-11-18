de3377917435f0a462a09f3dd0b9dbe8b3b2c583
KimHuiMang <piyrw9754@gmail.com>
2022-11-18 10:46:21 +0900

    다시 시작합니다

---
+++ src/main/java/jamong/calendo/CalendoApplication.java
@@ -0,0 +1,13 @@
+package jamong.calendo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalendoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalendoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jamong/calendo/CalendoApplicationTests.java
@@ -0,0 +1,13 @@
+package jamong.calendo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
